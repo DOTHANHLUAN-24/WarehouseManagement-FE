d3a27a00c799a971de7189138730fb1ad31e191e
Thanh Luan <thanhluando25@gmail.com>
2026-03-06 23:37:14 +0700

    🎨 feat(ui): setup MudBlazor for UI components

--- WarehouseManagement-FE/Program.cs
+++ WarehouseManagement-FE/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WarehouseManagement_FE;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,7 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7161/")
 });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
